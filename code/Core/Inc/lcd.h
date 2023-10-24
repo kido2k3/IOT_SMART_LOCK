3a042bdedea8411ab9c1dbd5ae7a5c64466d32cf
Kido <hoangphongvip2003@gmail.com>
2023-10-24 14:45:27 +0700

    modify lcd

--- code/Core/Inc/lcd.h
+++ code/Core/Inc/lcd.h
@@ -8,19 +8,17 @@
 #ifndef INC_LCD_H_
 #define INC_LCD_H_
 
-#include"main.h"
+#include "main.h"
 #define SLAVE_ADDRESS_LCD 0x4E
 #define TIME_OUT 100
 
-typedef struct{
-	I2C_HandleTypeDef* i2c;
+typedef struct
+{
+	I2C_HandleTypeDef *i2c;
+	int address;
 } lcd_led;
 extern lcd_led my_lcd;
 
-void lcd_init();
-void lcd_send_cmd(char cmd);
-void lcd_send_string(char *str);
-void lcd_put_cur(int row, int col);
-void lcd_send_data (char data);
-void lcd_clear();
-#endif /* INC_LCD_H_ */
+int lcd_display(lcd_led *my_lcd, char *line1, char *line2);
+int lcd_erase(lcd_led *my_lcd);
+int lcd_init(lcd_led *my_lcd, int address, I2C_HandleTypeDef *i2c);
